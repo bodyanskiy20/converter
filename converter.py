4055c25012c55c99a0cddb4b765981f0158c0b48
Bogdan Gladkov <bodyanskiy20@gmail.com>
2024-02-18 13:26:39 +0100

    Final commit

--- converter.py
+++ converter.py
@@ -4,3 +4,17 @@ def convert(amount, rate):
 def choose_rate():
     currency = input('Выберите в какую валюту хотите конвертировать гривны? (EUR, USD, PLN): ')
     return currency
+
+def converter():
+    amount = float(input('Введите сумму для конвертации: '))
+    currency = choose_rate()
+
+    conversion_rates = {'EUR': 0.024, 'USD': 0.026, 'PLN': 0.11}
+    if currency in conversion_rates:
+        rate = conversion_rates[currency]
+        result = convert(amount, rate)
+        print(f'Результат: {amount} гривен = {result} {currency}')
+    else:
+        print('Ошибка. Вы выбрали несуществующую валюту.')
+        
+converter()
